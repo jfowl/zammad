295f6f2a19be99c522cd5ec596234abbafc379e0
Rolf Schmidt <rolf.schmidt@zammad.com>
2023-03-24 14:03:52 +0000

    Update package installation to support custom packages.

--- contrib/packager.io/preinstall.sh
+++ contrib/packager.io/preinstall.sh
@@ -3,6 +3,14 @@
 # packager.io preinstall script
 #
 
+PATH=/opt/zammad/bin:/opt/zammad/vendor/bundle/bin:/sbin:/bin:/usr/sbin:/usr/bin:
+
+# import config
+. /opt/zammad/contrib/packager.io/config
+
+# import functions
+. /opt/zammad/contrib/packager.io/functions
+
 #
 # Make sure that after installation/update there can be only one sprockets manifest,
 #   the one coming from the package. The package manager will ignore any duplicate files
@@ -10,3 +18,6 @@
 #   These duplicates can cause the application to fail, however.
 #
 rm -f /opt/zammad/public/assets/.sprockets-manifest-*.json || true
+
+# remove local files of the packages
+zammad_packages_uninstall_all_files
